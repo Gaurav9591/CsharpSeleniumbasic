5d068b721541df16896ff80dc0450b0990545ca3
Gaurav9591 <33199297+Gaurav9591@users.noreply.github.com>
2019-12-10 10:53:57 +0530

    Add files via upload
    csharpbasic framework demo

---
+++ NUnit.Tests3/Base Test/BaseClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnit.Tests3.Base_Test
+{
+
+    public class BaseClass
+    {
+
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            // TODO: Add your test code here
+            driver.Url = "http://localhost:8007/ProLawPortal/#/Login";
+
+
+        }
+
+
+        [OneTimeTearDown]
+        public void close()
+        {
+
+            driver.Quit();
+        }
+
+
+
+
+    }
+}
---
+++ NUnit.Tests3/TestClass.cs
@@ -0,0 +1,34 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using NUnit.Tests3.Base_Test;
+
+namespace NUnit.Tests3
+{
+    [TestFixture]
+    public class TestClass : BaseClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+
+           
+            Thread.Sleep(5000);
+
+            // IWebElement element = driver.FindElement(By.XPath("(//input[contains(@class,'k-textbox login-input')])[1]"));
+
+            IWebElement element = driver.FindElement(By.CssSelector("#UserID"));
+
+            element.SendKeys("gaurav");
+          
+        
+        
+        
+        }
+    }
+}
